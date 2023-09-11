d0c342edcff798d9034c97eb20b7fc8b4af10895
victorhgb <victorhugo-brito@hotmail.com>
2023-09-11 15:44:40 -0300

    add project

---
+++ src/main/java/com/victorhgb/library/Topico.java
@@ -0,0 +1,8 @@
+package com.victorhgb.library;
+
+/**
+ * A nice enumeration of book topics
+ */
+public enum Topico {
+    COMPUTACAO, SCI_FI, FINANCAS, ROMANCE
+}
---
+++ src/test/java/com/victorhgb/library/ContextoDeTeste.java
@@ -0,0 +1,18 @@
+package com.victorhgb.library;
+
+public final class ContextoDeTeste {
+
+    public static final ContextoDeTeste INSTANCIA = new ContextoDeTeste();
+
+    private ServicoDeBiblioteca biblioteca;
+
+    public ServicoDeBiblioteca obtemServico(boolean carrega) {
+        biblioteca = ServicoDeBiblioteca.obtemServico(carrega);
+        return biblioteca;
+    }
+
+    public ServicoDeBiblioteca biblioteca() {
+        return biblioteca;
+    }
+
+}
